8b3b0e317a3dead1027a20ad59d6fcac5d0ecc05
Doua20 <douakona@gmail.com>
2024-04-18 13:15:23 +0200

    accueil

--- templates/accueil/index.php
+++ templates/accueil/index.php
@@ -1,1 +1,34 @@
-salut
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>accueil_fil_rouge_g3</title>
+    <link rel="stylesheet" href="./../assets/css/accueil.css">
+</head>
+<body>
+<div>
+    <h1>Bienvenue dans le Royaume des Légendes</h1>
+    <img src="./../assets/images/hero.jpg" alt="">
+    <h2>Royaume des Légendes : L'Odyssée des Gardiens</h2>
+    <p>Plongez dans un monde envoûtant où les légendes prennent vie et où
+         l'aventure vous appelle à chaque coin de rue. Bienvenue dans le "Royaume
+          des Légendes", un univers épique où les mystères ancestraux rencontrent
+           la magie la plus pure. Incarnez un Gardien, un protecteur choisi pour 
+           sauvegarder l'équilibre entre les forces du bien et du mal.</p>
+
+    <p>Dans ce jeu de rôle captivant, explorez des contrées vastes et variées,
+         depuis les sombres forêts hantées jusqu'aux majestueuses cités célestes. 
+         Affrontez des créatures mythiques, résolvez des énigmes antiques et forgez
+          des alliances avec d'autres Gardiens dans votre quête pour préserver la paix.</p>
+
+    <p>Préparez-vous à vivre une aventure inoubliable, où chaque choix que vous
+         ferez influencera le destin du royaume. Êtes-vous prêt à vous élever en tant
+          que Gardien et à écrire votre propre légende dans les annales de ce monde
+           fantastique ? Rejoignez-nous dès maintenant dans "L'Odyssée des Gardiens" !</p>
+</div>
+
+
+
+</body>
+</html>
