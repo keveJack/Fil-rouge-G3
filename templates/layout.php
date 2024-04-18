395f297c989635cfb3e2da513e9f4fcfae9f86e5
kevin <kevincooldu36@gmail.com>
2024-04-18 13:11:41 +0200

    layout

--- templates/layout.php
+++ templates/layout.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr-FR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+ <link rel="stylesheet" href="./assets/css/main.css">
 </head>
 <body>
+<header>
+    <div id="img_logo">&nbsp;</div>
+    <a href="#">Connexion</a>
+    <a href="#">Inscription</a>
+</header>
+
     <?=$content?>
+    <div class="footer">
+  <p>Copyright ©2024 My Website</p>
+</div>
 </body>
 </html>
